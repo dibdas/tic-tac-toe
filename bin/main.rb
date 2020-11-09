5dbe95e5779a5fbac55555205e260dd9f03bcfe9
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-09 18:18:07 +0545

    Update main.rb file with interface design.
    - User will be asked to choose the icon x or o to continue
    - User will get the board to choose from 1 - 9 value to place their move.

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,38 @@
-
 #!/usr/bin/env ruby
-puts 'Hello world!'
 
-puts '“Now, your move is displayed on the board”'
+player_one = nil
+
+loop do
+  print 'Choose icon from (x or o) to start playing: (x/o)?: '
+  player_one = gets.chomp.downcase
+
+  break if %w[x o].include?(player_one)
+end
+
+player_two = player_one == 'x' ? 'o' : 'x'
+
+puts player_two
+
+puts 'Choose your move from the board below:'
+
+puts '-' * 13
+print '| 1 |'
+print ' 2 |'
+print ' 3 |'
+puts ''
+puts '+---+---+---+'
+
+print '| 4 |'
+print ' 5 |'
+print ' 6 |'
+puts ''
+puts '+---+---+---+'
+
+print '| 8 |'
+print ' 7 |'
+print ' 9 |'
+puts ''
+puts '-' * 13
+
 input = gets.chomp
 puts input
- 
-print  "1"
-print " 2"

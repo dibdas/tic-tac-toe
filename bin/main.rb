0a6fc98e859ed884fa3576c37928bf0b9f0161e0
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-09 19:27:41 +0545

    Refactor main.rb file
    - instead of printing board with all print statement used array to achieve it.
    - update player_input var in choice with self.player_input
    - Fixes for linter error

--- bin/main.rb
+++ bin/main.rb
@@ -1,84 +1,77 @@
 #!/usr/bin/env ruby
-=begin
-player_one = nil
-
-
-puts ("player_one "+ player_one)
-puts ("player_two "+ player_two)
-
-puts 'Choose your move from the board below:'
-
-puts '-' * 13
-print '| 1 |'
-print ' 2 |'
-print ' 3 |'
-puts ''
-puts '+---+---+---+'
-
-print '| 4 |'
-print ' 5 |'
-print ' 6 |'
-puts ''
-puts '+---+---+---+'
-
-print '| 8 |'
-print ' 7 |'
-print ' 9 |'
-puts ''
-puts '-' * 13
-
-puts "chose any value from the board"
-player1_input = gets.chomp
-puts "chose any value from the board"
-player2_input = gets.chomp
-=end
+# player_one = nil
+#
+#
+# puts ("player_one "+ player_one)
+# puts ("player_two "+ player_two)
+#
+# puts 'Choose your move from the board below:'
+#
+# puts '-' * 13
+# print '| 1 |'
+# print ' 2 |'
+# print ' 3 |'
+# puts ''
+# puts '+---+---+---+'
+#
+# print '| 4 |'
+# print ' 5 |'
+# print ' 6 |'
+# puts ''
+# puts '+---+---+---+'
+#
+# print '| 8 |'
+# print ' 7 |'
+# print ' 9 |'
+# puts ''
+# puts '-' * 13
+#
+# puts "chose any value from the board"
+# player1_input = gets.chomp
+# puts "chose any value from the board"
+# player2_input = gets.chomp
 
 class TicTacToe
-    attr_reader :icon
-    def initialize(icon)
-        @icon=icon
-        @player_input = nil
+  attr_reader :icon
+  attr_accessor :player_input
+
+  def initialize(icon)
+    @icon = icon
+    @player_input = nil
+    @board_array = [['| 1 |', ' 2 |', ' 3 |'], ['| 4 |', ' 5 |', ' 6 |'], ['| 7 |', ' 8 |', ' 9 |']]
+  end
+
+  def display
+    puts '-' * 13
+    @board_array.each do |value|
+      value.each do |i|
+        print i
+      end
+      puts ''
+      puts '+---+---+---+'
     end
-    
-    def display
-        puts '-' * 13
-        print '| 1 |'
-        print ' 2 |'
-        print ' 3 |'
-        puts ''
-        puts '+---+---+---+'
+  end
 
-        print '| 4 |'
-        print ' 5 |'
-        print ' 6 |'
-        puts ''
-        puts '+---+---+---+'
-
-        print '| 8 |'
-        print ' 7 |'
-        print ' 9 |'
-        puts ''
-        puts '-' * 13
-
-    end
-    def choice
-        puts "chose any value from the board"
-        player_input = gets.chomp
-    end
-
+  def choice
+    puts 'chose any value from the board'
+    self.player_input = gets.chomp
+  end
 end
 
 print 'Choose icon from (x or o) to start playing: (x/o)?: '
 playerchoice = gets.chomp
 
-
 player_one = TicTacToe.new(playerchoice)
 puts player_one.icon
-player_2 = player_one.icon =='x' ? 'o': 'x'
-puts player_2
-player_two = TicTacToe.new(player_2)
+player2 = player_one.icon == 'x' ? 'o' : 'x'
+puts player2
+player_two = TicTacToe.new(player2)
 player_one.display
 player_one.choice
+puts "player one choice #{player_one.player_input}"
+
 player_two.display
 player_two.choice
- 
+puts "player two choice #{player_two.player_input}"
+
+puts "player one #{player_one}.\nplayer two #{player_two}"

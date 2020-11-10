573ab4571b2d9a558eb98c44d9cf838713bfded0
dibdas <dibyandu08das@gmail.com>
2020-11-10 18:47:37 +0530

    added player.rb
    upadte user input name and symbol

--- bin/main.rb
+++ bin/main.rb
@@ -86,9 +86,22 @@ array = [
 game_on = true
 
 winner = true
+def get_name
+  puts "enter a valid name"
+  name = gets.chomp
+  return name unless name.empty?
+end
+def get_symbol
+  puts "enter the symbol (x or o)"
+  symbol = gets.chomp
+  return symbol
+end
 
-player_one = Player.new()
-player_two = Player.new()
+player_one = Player.new(get_name,get_symbol)
+puts "player one: #{player_one.name}: #{player_one.symbol}"
+symbol_for_two = player_one.symbol =='x'? 'o' : 'x'
+player_two = Player.new(get_name,symbol_for_two)
+puts "player one: #{player_two.name}: #{symbol_for_two}"
 
 while game_on
   game_on = false if winner
--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,10 @@
 class Player
-
+    attr_reader :name ,:moves, :symbol
+  def initialize(name,symbol)
+    @name = name
+    @symbol = symbol
+    @moves =[]
+  end
 end
+
+

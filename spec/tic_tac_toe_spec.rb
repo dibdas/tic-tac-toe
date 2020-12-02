fcb68640262cde857697f74434e559472522541f
dibdas <dibyandu08das@gmail.com>
2020-12-02 14:05:20 +0530

    updated
    added draw

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -28,4 +28,9 @@ describe Board do
       expect(board.cells).to eql({ 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 })
     end
   end
+  describe '#draw' do
+    it 'should draw the board' do
+    expect(board.cells).to eql({1=>1, 2=>2, 3=>3, 4=>4, 5=>5, 6=>6, 7=>7, 8=>8, 9=>9})
+    end
+  end
 end
